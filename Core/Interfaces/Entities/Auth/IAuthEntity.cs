d4844f3bc0bcae2877c44a615b3eeb1753dc159d
Hacktin <isues41@gmail.com>
2020-09-25 11:01:57 +0300

    Entity interfaces have been created

---
+++ Core/Interfaces/Entities/Auth/IAuthEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces.Entities.Auth
+{
+    public interface IAuthEntity:IEntity
+    {
+        dynamic InternalToken { get; set; }
+    }
+}
