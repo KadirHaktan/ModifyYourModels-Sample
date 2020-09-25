669434e86b4ba17626cb3ff9e93f7e1500d36bff
Hacktin <isues41@gmail.com>
2020-09-25 11:01:16 +0300

    test Auth controller has been created

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces.Adapters;
+using Entities.Concerete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace API.Controllers
+{
+    
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthServiceAdapter _authServiceAdapter;
+
+        public AuthController(IAuthServiceAdapter authServiceAdapter)
+        {
+            this._authServiceAdapter = authServiceAdapter;
+
+            
+        }
+
+        
+        [HttpGet("api/forge/auth/token")]
+        public async Task<dynamic> GetToken()
+        {
+            return await _authServiceAdapter.GetAccessTokenAdapterAsync();
+        }
+    }
+}
