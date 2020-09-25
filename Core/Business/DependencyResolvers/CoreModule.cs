9d5c5f81ed6384fab39e32130e9eb28b1a8cf236
Hacktin <isues41@gmail.com>
2020-09-25 11:02:35 +0300

    Business's dependencies have been done

---
+++ Core/Business/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Utilities.Ioc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Business.DependencyResolvers
+{
+    public class CoreModule : ICoreModules
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Latest)
+                .AddNewtonsoftJson();
+
+        }
+    }
+}
