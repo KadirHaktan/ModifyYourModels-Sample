3aa49edaa6583e6d122a7ef7267ae61e84c5d243
Hacktin <isues41@gmail.com>
2020-09-25 11:03:24 +0300

    Statment runner methods have been written.But ı have not use these methods yet

---
+++ Core/Business/BusinessRunners.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Business
+{
+    public static class BusinessRunners
+    {
+    //    public static dynamic StatmentRunner(bool statment, dynamic method1, dynamic method2)
+    //    {
+    //        if (statment)
+    //        {
+    //            return method1;
+    //        }
+
+    //        else
+    //        {
+    //            return method2;
+    //        }
+    //    }
+
+    //    public static T StatmentRunner<T>(bool statment, T method1, T method2)
+    //    {
+    //        if (statment)
+    //        {
+    //            return method1;
+    //        }
+
+    //        else
+    //        {
+    //            return method2;
+    //        }
+    //    }
+
+    //    public static async Task<T> StatmentRunnerAsync<T>(bool statment, Task<T> method1, Task<T> method2)
+    //    {
+    //        if (statment)
+    //        {
+    //            return await method1;
+    //        }
+
+    //        else
+    //        {
+    //            return await method2;
+    //        }
+    //    }
+
+    //    public static async Task<dynamic> StatmentRunnerAsync(bool statment, Task<dynamic> method1, Task<dynamic> method2)
+    //    {
+    //        if (statment)
+    //        {
+    //            return await method1;
+    //        }
+
+    //        else
+    //        {
+    //            return await method2;
+    //        }
+    //    }
+
+
+    //}
+}
