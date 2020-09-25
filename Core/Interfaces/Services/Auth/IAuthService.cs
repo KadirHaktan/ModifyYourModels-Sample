8c9e21242ba28b8e6cb6d6460fc256a78c76e810
Hacktin <isues41@gmail.com>
2020-09-25 11:14:26 +0300

    auth service interface's folder has been edited

--- Core/Interfaces/Services/Auth/IAuthService.cs
+++ Core/Interfaces/Services/Auth/IAuthService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Core.Interfaces.Entities.Auth;
 
-namespace Core.Interfaces.Auth
+
+namespace Core.Interfaces.Services.Auth
 {
     public interface IAuthService
     {
