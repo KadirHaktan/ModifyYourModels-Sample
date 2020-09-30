3ebb60d0de0d116a9dd77801597ceecc671d4af7
Hacktin <isues41@gmail.com>
2020-09-30 16:20:05 +0300

    Generic interface repositories have been created

---
+++ Core/Interfaces/Repository/IIEntityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Interfaces.Entities;
+
+namespace Core.Interfaces.Repository
+{
+    public interface IIEntityRepository<T>:IRepository<T> where T:class,IEntity
+    {
+    }
+}
---
+++ Core/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        
+        Task<T> CreateAsync(T entity);
+
+        T Create(T entity);
+
+        Task CreateVoidAsync(T entity);
+
+        void CreateVoid(T entity);
+
+    }
+}
