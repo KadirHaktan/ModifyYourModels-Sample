2676c606a2ec90debfa101c5be44985f1f01fd30
Hacktin <isues41@gmail.com>
2020-09-30 16:19:20 +0300

    AppBundleServiceAdapters have been created

---
+++ Services/Adapters/Abstract/IAppBundleServiceAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Abstract;
+
+namespace Services.Adapters.Abstract
+{
+    public interface IAppBundleServiceAdapter<T> where T:class,IAppBundle
+    {
+        Task<dynamic> CreateAppBundleAsync(T appBundle, string localBundlesFolder);
+    }
+}
---
+++ Services/Adapters/Concerete/AppBundleServiceAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Abstract;
+using Services.Abstract;
+using Services.Adapters.Abstract;
+
+namespace Services.Adapters.Concerete
+{
+    public class AppBundleServiceAdapter<T> : IAppBundleServiceAdapter<T> where T : class, IAppBundle
+    {
+        private readonly IAppBundleService<T> _appBundleService;
+
+        public AppBundleServiceAdapter(IAppBundleService<T> appBundleService)
+        {
+            this._appBundleService = appBundleService;
+        }
+        public async Task<dynamic> CreateAppBundleAsync(T appBundle, string localBundlesFolder)
+        {
+            return await _appBundleService.CreateAppBundle(appBundle, localBundlesFolder);
+        }
+    }
+}
