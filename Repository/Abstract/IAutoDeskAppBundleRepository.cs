6ce756f82cc45bb5e9c52a0491d3cc99a7276b87
Hacktin <isues41@gmail.com>
2020-09-30 16:18:27 +0300

    Create inteface and class repositories for AutoDesk App Bundle methods

---
+++ Repository/Abstract/IAutoDeskAppBundleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Forge.DesignAutomation.Model;
+using Core.Interfaces.Repository;
+
+namespace Repository.Abstract
+{
+    public interface IAutoDeskAppBundleRepository:IRepository<AppBundle>
+    {
+
+
+        Task<Alias> ModifyAsync(string appBundleName,string Alias,AliasPatch aliasSpec);
+
+        Task<Alias> CreateAppBundleAsync(string appBundleName, Alias aliasSpec);
+
+        Task<dynamic> CreateAppBundleVersionAsync(string appBundleName, AppBundle appBundleSpec);
+
+        Task<Page<string>> GetAppBundles(string? page);
+
+
+    }
+}
---
+++ Repository/Concerete/AutoDeskAppBundleRepository.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Autodesk.Forge.DesignAutomation;
+using Autodesk.Forge.DesignAutomation.Model;
+using Repository.Abstract;
+
+namespace Repository.Concerete
+{
+    public class AutoDeskAppBundleRepository:IAutoDeskAppBundleRepository
+    {
+        private readonly DesignAutomationClient _designAutomation=new DesignAutomationClient();
+
+        public AppBundle Create(AppBundle entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Alias> CreateAppBundleAsync(string appBundleName, Alias aliasSpec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<dynamic> CreateAppBundleVersionAsync(string appBundleName, AppBundle appBundleSpec)
+        {
+            return await _designAutomation.CreateAppBundleVersionAsync(appBundleName, appBundleSpec);
+        }
+
+        public async Task<AppBundle> CreateAsync(AppBundle entity)
+        {
+            return await _designAutomation.CreateAppBundleAsync(entity);
+        }
+
+        public void CreateVoid(AppBundle entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateVoidAsync(AppBundle entity)
+        {
+            await _designAutomation.CreateAppBundleAsync(entity);
+        }
+
+        public async Task<Page<string>> GetAppBundles(string? page)
+        {
+            return await _designAutomation.GetAppBundlesAsync(page);
+        }
+
+
+        public async Task<Alias> ModifyAsync(string appBundleName, string Alias, AliasPatch aliasSpec)
+        {
+            return await _designAutomation.ModifyAppBundleAliasAsync(appBundleName, Alias, aliasSpec);
+        }
+    }
+}
