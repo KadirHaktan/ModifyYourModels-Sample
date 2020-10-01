b7fc12ba55754dc42dd5220a86c76ea1ea6db2e7
Hacktin <isues41@gmail.com>
2020-10-01 14:52:54 +0300

    restSharp library helper has been created

---
+++ API/Helpers/Http/Restsharp/RestSharpHttpEnvoirmentHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace API.Helpers.Http.Restsharp
+{
+    public static class RestSharpHttpEnvoirmentHelper
+    {
+        public static async Task CreateHttpFormDataEnvoirment(string url, Method method, dynamic FormData, string zipFileName, string cacheControl)
+        {
+            RestClient uploadClient = new RestClient(url);
+            RestRequest request = new RestRequest(string.Empty, method);
+            request.AlwaysMultipartFormData = true;
+            foreach (KeyValuePair<string, string> x in FormData)
+                request.AddParameter(x.Key, x.Value);
+            request.AddFile("file", zipFileName);
+            request.AddHeader("Cache-Control", cacheControl);
+            await uploadClient.ExecuteTaskAsync(request);
+        }
+    }
+}
