7536c57646b8712a647851429d2cd6bb16f2c33b
Hacktin <isues41@gmail.com>
2020-10-01 14:53:13 +0300

    engine service adapters have been created

---
+++ Services/Adapters/Abstract/IEngineServiceAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Adapters.Abstract
+{
+    public interface IEngineServiceAdapter
+    {
+        Task<List<string>> GetEnginesToStringAdapter();
+    }
+}
---
+++ Services/Adapters/Concerete/EngineServiceAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Services.Abstract;
+using Services.Adapters.Abstract;
+
+namespace Services.Adapters.Concerete
+{
+    public class EngineServiceAdapter : IEngineServiceAdapter
+    {
+        private readonly IEngineServices _engineServices;
+
+        public EngineServiceAdapter(IEngineServices engineServices)
+        {
+            this._engineServices = engineServices;
+        }
+        public  async Task<List<string>> GetEnginesToStringAdapter()
+        {
+            return await _engineServices.GetAvailableEnginesToString();
+        }
+    }
+}
