70035b82574d53a72e007bac501fc3059153a669
Hacktin <isues41@gmail.com>
2020-10-01 14:52:35 +0300

    basic signal r hub has been created

---
+++ API/Hubs/DesignAutomationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.Hubs
+{
+    public class DesignAutomationHub:Hub
+    {
+        public string ConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+    }
+}
