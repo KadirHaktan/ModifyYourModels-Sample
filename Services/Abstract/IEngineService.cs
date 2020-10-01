6b90e768388fa66155d107e5c9e020b1f1b38ec1
Hacktin <isues41@gmail.com>
2020-10-01 14:54:11 +0300

    engine services have been created

---
+++ Services/Abstract/IEngineService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstract
+{
+   public interface IEngineServices
+   {
+       Task<List<string>> GetAvailableEnginesToString();
+   }
+}
---
+++ Services/Concerete/AutoDeskEngineService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Forge.DesignAutomation.Model;
+using Core.Interfaces.Adapters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Abstract;
+using Services.Abstract;
+
+namespace Services.Concerete
+{
+    public class AutoDeskEngineService : IEngineServices
+    {
+        private readonly IAuthServiceAdapter _authServiceAdapter;
+
+        private readonly IAutoDeskAppBundleRepository _repository;
+        public AutoDeskEngineService(IAuthServiceAdapter authServiceAdapter,IAutoDeskAppBundleRepository repository)
+        {
+            this._authServiceAdapter = authServiceAdapter;
+            this._repository = repository;
+        }
+        public async Task<List<string>> GetAvailableEnginesToString()
+        {
+            dynamic token = await _authServiceAdapter.GetAccessTokenAdapterAsync();
+
+            Page<string> engines = await _repository.GetAppEngines(null);
+
+            engines.Data.Sort();
+
+            return engines.Data;
+        }
+    }
+}
