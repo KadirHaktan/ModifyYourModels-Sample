a4cf6adff8f27a66a1d76c68c82816ef6cedbe32
Hacktin <isues41@gmail.com>
2020-10-13 21:58:40 +0300

    entity dll has been remove

---
+++ Entities/Concerete/StartupWorkItemInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Entities.Concerete
+{
+    public class StartupWorkItemInput
+    {
+        public IFormFile inputFile { get; set; }
+        public string data { get; set; }
+    }
+}
