b792216b2317b1f590ae2d5f59b6e2c9453a86cb
Hacktin <isues41@gmail.com>
2020-10-13 21:56:22 +0300

    work item adapters have been created

---
+++ Services/Adapters/Abstract/IWorkItemServiceAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Services.Adapters.Abstract
+{
+    public interface IWorkItemServiceAdapter
+    {
+        Task<dynamic> StartWorkItem(StartWorkItemInput input, string rootPath);
+
+    }
+}
---
+++ Services/Adapters/Concerete/WorkItemServiceAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Services.Abstract;
+using Services.Adapters.Abstract;
+
+namespace Services.Adapters.Concerete
+{
+    public class WorkItemServiceAdapter : IWorkItemServiceAdapter
+    {
+        private readonly IWorkItemService _service;
+
+        public WorkItemServiceAdapter(IWorkItemService service)
+        {
+            this._service = service;
+
+        }
+        public async Task<dynamic> StartWorkItem(StartWorkItemInput input, string rootPath)
+        {
+            return await _service.StartWorkItem(input, rootPath);
+        }
+    }
+}
