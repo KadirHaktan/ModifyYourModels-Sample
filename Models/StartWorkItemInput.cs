d5d982d7dcf744d97b67ec7d6e7496617547f33f
Hacktin <isues41@gmail.com>
2020-10-13 21:58:26 +0300

    start work ıtem input model has been created

---
+++ Models/StartWorkItemInput.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class StartWorkItemInput
+    {
+        public IFormFile inputFile { get; set; }
+        public string data { get; set; }
+    }
+}
