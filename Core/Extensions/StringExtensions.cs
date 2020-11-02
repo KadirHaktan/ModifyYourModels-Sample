f7a3de60085855ca6e5be2fec6c00ccd5ee41537
Hacktin <isues41@gmail.com>
2020-11-02 14:43:59 +0300

    string replacing for adapt to  regular expression rule

---
+++ Core/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplyToCharToEnglishFormat(this string variable)
+        {
+
+            return variable.Replace('ı', 'i')
+                .Replace('ü', 'u')
+                .Replace('ğ', 'g');
+        }
+
+      
+    }
+}
